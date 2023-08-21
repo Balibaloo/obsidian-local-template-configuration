c0752b66a814f7976c33797939f8b0155193914b
Roman Kubiv <romankubiv101@gmail.com>
2023-08-21 21:30:53 +0100

    Fix catch modal cancel error

--- src/template_variables.ts
+++ src/template_variables.ts
@@ -7,7 +7,11 @@ export async function getVariableValues(app:App, variables:TemplateVariable[]) {
   for (let variable of variables) {
     let val;
     if (variable.type == TemplateVariableType.text) {
+      try {
         val = await GenericInputPrompt.Prompt(app, variable.name, undefined, undefined, variable.required);
+      } catch {}
+      
+      if ( variable.required && (val === "" || val === undefined || val === null)){
         return gatheredValues;
       }
 		}
