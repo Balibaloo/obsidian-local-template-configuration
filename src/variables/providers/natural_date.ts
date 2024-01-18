b32cc77c5b89f692043388d4219dab1f3deed5e5
Roman Kubiv <romankubiv101@gmail.com>
2024-01-18 20:56:45 +0000

    Add natural date before and after validation

--- src/variables/providers/natural_date.ts
+++ src/variables/providers/natural_date.ts
@@ -3,17 +3,38 @@ import { GenericInputPrompt } from "../suggest";
 import { TemplateVariable } from "../templateVariables";
 
 export type TemplateVariableVariables_NaturalDate = {
+  after?: string,
+  before?: string,
 };
 
-export const parseNaturalDateVariableFrontmatter = (fm: any) => ({
-});
+export const parseNaturalDateVariableFrontmatter = (fm: any) => {
+  const NLDates = (app as any).plugins.getPlugin("nldates-obsidian");
+
+  const dateVariable:TemplateVariableVariables_NaturalDate = {
+    after: fm.after,
+    before: fm.before,
+  }
+  
+  if (dateVariable.after && !NLDates.parseDate(dateVariable.after).moment.isValid()){
+    throw new Error(`Error: Intent variable ${fm.name}, date after property, ${dateVariable.after} is not a valid natural language date.`);
+  };
+
+  if (dateVariable.before && !NLDates.parseDate(dateVariable.before).moment.isValid()){
+    throw new Error(`Error: Intent variable ${fm.name}, date before property, ${dateVariable.before} is not a valid natural language date.`);
+  };
+
+  return dateVariable;
+};
 
 export async function getNaturalDateVariableValue(variable: TemplateVariable&TemplateVariableVariables_NaturalDate, existingValue:string): Promise<string>{
   if (!validateNaturalDate(variable, existingValue, false)) {
     try {
       existingValue = await GenericInputPrompt.Prompt(app, variable,
         text => validateNaturalDate(variable, text, false),
-        "Error: Please enter a valid natural language date");
+        "Error: Please enter a valid natural language date"
+          + (variable.after ? ` after ${variable.after}` : "")
+          + (variable.before && variable.after ? " and" : "")
+          + (variable.before ? ` before ${variable.before}` : ""));
     } catch (e){
       console.log(e);
     }
@@ -34,7 +55,19 @@ function validateNaturalDate(variable: TemplateVariable & TemplateVariableVariab
   const parsedDate = NLDates.parseDate(val);
   if (!parsedDate.moment.isValid()) {
     if (variable.required && throwErrors)
-      throw new Error(`Error: missing required date variable ${variable.name}`);
+      throw new Error(`Error: The date entered for ${variable.name} is not valid`);
+    return false;
+  }
+
+  if (variable.after && !parsedDate.moment.isAfter(NLDates.parseDate(variable.after).moment)) {
+    if (variable.required && throwErrors)
+      throw new Error(`Error: The date entered for ${variable.name}, (${parsedDate.formattedString}) "${val}" must be before "${NLDates.parseDate(variable.after).moment }" (${variable.after})`);
+    return false;
+  }
+
+  if (variable.before && !parsedDate.moment.isBefore(NLDates.parseDate(variable.before).moment)) {
+    if (variable.required && throwErrors)
+      throw new Error(`Error: The date entered for ${variable.name}, (${parsedDate.formattedString}) "${val}" must be after "${NLDates.parseDate(variable.before).moment }" (${variable.before})`);
     return false;
   }
 
