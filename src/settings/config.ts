c7a3d8bec657d8cccc121798cf6fd88965d8edb9
Roman Kubiv <romankubiv101@gmail.com>
2024-05-13 04:22:00 +0100

    Fix make tab default new note show style

--- src/settings/config.ts
+++ src/settings/config.ts
@@ -10,7 +10,7 @@ export const DEFAULT_SETTINGS: PTSettings = {
   intentNotesFilterSetName: "default",
   selectionDelimiters: ",|",
   showNewNotes: true,
-	showNewNotesStyle: "split",
+	showNewNotesStyle: "tab",
 	showNewMultiNotes: true,
 }
 
